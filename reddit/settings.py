68d6b5e7173150c517418403c156aa04e3a6f2f5
Joe Davison <joedavison98@gmail.com>
2023-01-09 13:19:14 +0000

    S3 static files

--- reddit/settings.py
+++ reddit/settings.py
@@ -22,12 +22,12 @@ IS_HEROKU = "DYNO" in os.environ
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
-
 if IS_HEROKU:
     ALLOWED_HOSTS = ['jreddit.herokuapp.com', 'jreddit.joedavison.uk']
+    DEBUG = False
 else:
     ALLOWED_HOSTS = ['192.168.1.121', '127.0.0.1', 'localhost']
+    DEBUG = True
 
 # Application definition
 
@@ -130,14 +130,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -168,12 +160,29 @@ SECRETBALLOT_FOR_MODELS = {
     'posts.comment': {},
 }
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+
+# AWS config
+
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=6400'}
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+if IS_HEROKU:
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, 'static')
+else:
+    STATIC_URL = '/static/'
 
 AWS_S3_REGION_NAME = 'eu-west-2'
 
